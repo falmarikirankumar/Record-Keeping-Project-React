1302dc3b6f0e185619aef8d4abc006086a59a604
Kirankumar Falmari <falmarikirankumar@gamil.com>
2023-01-22 00:06:55 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [data, setData] = useState([]);
+
+  const addData = () => {
+    // setData({ name: name, email: email });
+    setData([...data, { name, email }]);
+    setName("");
+    setEmail("");
+  };
+  const removeItem = (index) => {
+    let arr = data;
+    arr.splice(index, 1);
+    setData([...arr]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="form">
+        <Stack direction="row" spacing={2}>
+          <TextField
+            id="outlined-basic"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            label="Name"
+            variant="outlined"
+          />
+          <TextField
+            id="outlined-basic"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email"
+            variant="outlined"
+          />
+          <Button color="success" onClick={addData} variant="contained">
+            <AddIcon />
+          </Button>
+        </Stack>
+      </div>
+
+      <div className="data">
+        <div className="data_val">
+          <h4>Name</h4>
+          <h4>Email</h4>
+          <h4>Remove</h4>
+        </div>
+        {data.map((element, index) => {
+          return (
+            <div key={index} className="data_val">
+              <h4>{element.name}</h4>
+              <h4>{element.email}</h4>
+              <Stack>
+                <Button
+                  onClick={() => removeItem(index)}
+                  variant="contained"
+                  color="error"
+                >
+                  <DeleteIcon />
+                </Button>
+              </Stack>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/Components/Fields.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+const Fields = ({ name, email, index }) => {
+  return <></>;
+};
+
+export default Fields;
+
+// {/* <Fields
+//               key={index}
+//               name={element.name}
+//               email={element.email}
+//               index={index}
+//             /> */}
